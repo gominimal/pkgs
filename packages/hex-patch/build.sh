fdd9106e35d6290b350eebf221383d9ccb678644
Tom <tom@minimal.dev>
2025-10-16 20:46:04 -0700

    hex-patch: package hex-patch 1.12.4

---
+++ packages/hex-patch/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ex
+export CC=gcc
+export LD=gcc
+export RUSTFLAGS="-C linker=gcc"
+
+cargo build --release
+
+mkdir -p $OUTPUT_DIR/usr/bin
+ls -lah target/release
+cp target/release/hex-patch $OUTPUT_DIR/usr/bin
