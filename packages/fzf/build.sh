5c33e321edd60fe430f1d7b734d23bb8c051f1a7
Tom <tom@minimal.dev>
2025-10-17 12:43:39 -0700

    fzf: package fzf 0.66.0

---
+++ packages/fzf/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ex
+
+export FZF_VERSION=$MINIMAL_ARG_VERSION
+export FZF_REVISION=tarball
+
+export GOROOT=/usr/go
+
+make
+make install
+install -D -m 0755 bin/fzf "$OUTPUT_DIR/usr/bin/fzf"
+
+install -D -m 0755 shell/completion.bash "$OUTPUT_DIR/usr/share/bash-completion/fzf"
