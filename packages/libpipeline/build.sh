d748815940685cf10ebee7e60fc5b9dbd038d4ef
Tom <tom@minimal.dev>
2025-10-17 20:14:38 -0700

    libpipeline,man-db: use release tarballs to remove need to run bootstrap

--- packages/libpipeline/build.sh
+++ packages/libpipeline/build.sh
@@ -4,10 +4,8 @@ set -ex
 tar xf libpipeline-1.5.8.tar.gz
 cd libpipeline-1.5.8
 
-./bootstrap
 ./configure  --prefix=/usr     \
              --disable-static
 
 make -j$(nproc)
-DESTDIR=$OUTPUT_DIR make DESTDIR="$OUTPUT_DIR" -j$(nproc) install
-
+make DESTDIR="$OUTPUT_DIR" install
--- packages/man-db/build.sh
+++ packages/man-db/build.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 set -ex
 
-tar xf man-db-2.13.1.tar.gz
+tar xf man-db-2.13.1.tar.xz
 cd man-db-2.13.1
-
-git init # bootstrap complains w/o parent git repo for gnulib checkout
-
-# fails with
-#  useradd: Permission denied.
-#  useradd: cannot lock /etc/passwd; try again later.
-#useradd -U man  # for final install with user and group for "man"
-
-./bootstrap # super slow due to git checkout of gnulib
 
 ./configure  --prefix=/usr     \
              --disable-setuid # gets around lack of useradd, but man page cache not updated by users using man
