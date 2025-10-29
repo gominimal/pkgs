8084a98355d841a81d8e6230c8446df0d091f145
Tom <tom@minimal.dev>
2025-10-29 15:25:24 -0700

    gettext: ellude tests, which are slowwww

--- packages/gettext/build.sh
+++ packages/gettext/build.sh
@@ -4,11 +4,11 @@ set -e
 tar xfo gettext-0.26.tar.xz
 cd gettext-0.26
 
-./configure  --prefix=/usr     \
-            --disable-static \
+./configure  --prefix=/usr      \
+            --disable-static    \
             --docdir=/usr/share/doc/gettext-0.26
 
 make -j$(nproc)
-make check
+# make check # TODO: Move somewhere else
 make DESTDIR="$OUTPUT_DIR" install
 chmod -v 0755 "$OUTPUT_DIR/usr/lib/preloadable_libintl.so"
