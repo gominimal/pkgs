89c12c6cf8ebc626edab378bf41bc1997485c1de
Tom <tom@minimal.dev>
2025-11-05 12:49:23 -0800

    pnpm: package pnpm 10.15.1

---
+++ packages/pnpm/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+tar xf "pnpm-${MINIMAL_ARG_VERSION}.tgz"
+cd package
+
+install -d $OUTPUT_DIR/usr/{bin,libexec}
+cp -R . $OUTPUT_DIR/usr/libexec/pnpm
+ln -s ../libexec/pnpm/bin/pnpm.cjs $OUTPUT_DIR/usr/bin/pnpm
+ln -s ../libexec/pnpm/bin/pnpx.cjs $OUTPUT_DIR/usr/bin/pnpx
