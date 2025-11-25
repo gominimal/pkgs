3692ae5caf2c5a599360c0350784c82a33bb31ff
Tom <tom@minimal.dev>
2025-11-25 14:33:09 -0800

    clipper2: package clipper 1.5.4

---
+++ packages/clipper2/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+cd CPP
+
+mkdir build &&
+cd    build
+
+cmake -D CMAKE_INSTALL_PREFIX=/usr          \
+      -D CMAKE_INSTALL_LIBDIR=/usr/lib      \
+      -D CMAKE_BUILD_TYPE=Release           \
+      -D CLIPPER2_EXAMPLES=OFF              \
+      -D CLIPPER2_TESTS=OFF                 \
+      -D BUILD_SHARED_LIBS=ON               \
+      -G Ninja ..
+
+ninja
+
+DESTDIR="$OUTPUT_DIR" ninja install
