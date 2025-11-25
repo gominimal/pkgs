be39b422b417fae692457ba40f46c3470a230808
Tom <tom@minimal.dev>
2025-11-25 15:02:39 -0800

    manifold: package manifold 3.3.2

---
+++ packages/manifold/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+mkdir build &&
+cd    build
+
+cmake -D CMAKE_INSTALL_PREFIX=/usr          \
+      -D CMAKE_INSTALL_LIBDIR=/usr/lib      \
+      -D CMAKE_BUILD_TYPE=Release           \
+      -D MANIFOLD_CROSS_SECTION=ON          \
+      -D MANIFOLD_PAR=TBB                   \
+      -D MANIFOLD_TEST=OFF                  \
+      -D BUILD_SHARED_LIBS=ON               \
+      -G Ninja ..
+
+ninja
+
+DESTDIR="$OUTPUT_DIR" ninja install
