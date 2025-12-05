857ea6c8ba156a000c7e76c54ce195ecf8c18edc
Tom <tom@minimal.dev>
2025-12-04 16:39:42 -0800

    openssh: package openssh 10.2p1

---
+++ packages/openssh/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+./configure --prefix=/usr                            \
+            --sysconfdir=/etc/ssh                    \
+            --with-privsep-path=/var/lib/sshd        \
+            --with-default-path=/usr/bin             \
+            --with-superuser-path=/usr/sbin:/usr/bin \
+            --with-pid-dir=/run
+make -j $(nproc)
+
+DESTDIR="$OUTPUT_DIR" make install
