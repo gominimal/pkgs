2af5086fd75f78b564f46119d02ff4b6048029f0
Tom <tom@minimal.dev>
2025-12-15 10:55:19 -0800

    libunistring: bump version to 1.4.1

--- packages/libunistring/build.sh
+++ packages/libunistring/build.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -e
 
-tar xfo libunistring-1.3.tar.xz
-cd libunistring-1.3
-
-./configure  --prefix=/usr           \
-            --disable-static       \
+./configure  --prefix=/usr          \
+            --disable-static        \
             --docdir=/usr/share/doc/libunistring-1.3
 
 make -j$(nproc)
