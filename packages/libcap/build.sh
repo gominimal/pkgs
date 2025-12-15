468250efdcc662e7dad667084a21a948a8d6761e
Tom <tom@minimal.dev>
2025-12-15 10:54:25 -0800

    libcap: bump version to 2.77

--- packages/libcap/build.sh
+++ packages/libcap/build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-tar xfo libcap-2.76.tar.xz
-cd libcap-2.76
-
 sed -i '/install -m.*STA/d' libcap/Makefile
 
 # TODO: Remove once /usr/bin/bash shows up in the bash build-spec output
