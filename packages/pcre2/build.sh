abd41247df6264064e085d4cd292e4045c054371
Tom <tom@minimal.dev>
2025-12-15 11:21:01 -0800

    pcre2: bump version to 10.47

--- packages/pcre2/build.sh
+++ packages/pcre2/build.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -e
 
-tar xfo pcre2-10.46.tar.bz2
-cd pcre2-10.46
+tar xfo "pcre2-${MINIMAL_ARG_VERSION}.tar.bz2"
+cd "pcre2-${MINIMAL_ARG_VERSION}"
 
 # TODO
-# --enable-pcre2test-libreadline 
+# --enable-pcre2test-libreadline
 
-./configure  --prefix=/usr                        \
-            --docdir=/usr/share/doc/pcre2-10.45 \
-            --enable-unicode                    \
-            --enable-jit                        \
-            --enable-pcre2-16                   \
-            --enable-pcre2-32                   \
-            --enable-pcre2grep-libz             \
-            --enable-pcre2grep-libbz2           \
+./configure  --prefix=/usr                          \
+            --docdir=/usr/share/doc/pcre2-10.45     \
+            --enable-unicode                        \
+            --enable-jit                            \
+            --enable-pcre2-16                       \
+            --enable-pcre2-32                       \
+            --enable-pcre2grep-libz                 \
+            --enable-pcre2grep-libbz2               \
             --disable-static
 
 make -j$(nproc)
