3a486530f5a0e8a68353dc107d0aa721e72b7a2b
Tom <tom@minimal.dev>
2025-12-15 11:16:00 -0800

    meson: bump version to 1.10.0

--- packages/meson/build.sh
+++ packages/meson/build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-tar xf meson-1.9.0.tar.gz
-cd meson-1.9.0
-
 pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
 
 pip3 install --root $OUTPUT_DIR --no-index --find-links dist meson
