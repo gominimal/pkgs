dd545ced796cbab40251d7e865c227b8cc2e90b4
Tom <tom@minimal.dev>
2025-12-15 10:55:36 -0800

    libxcrypt: bump version to 4.5.2

--- packages/libxcrypt/build.sh
+++ packages/libxcrypt/build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-tar xfo libxcrypt-4.4.38.tar.xz
-cd libxcrypt-4.4.38
-
 ./configure --prefix=/usr \
             --enable-hashes=strong,glibc \
             --enable-obsolete-api=no \
