100e6a3530e4be81c6f2d0d3700358c6f7df1d00
Tom <tom@minimal.dev>
2025-12-22 12:45:50 -0800

    nano: fix output doc directory

--- packages/nano/build.sh
+++ packages/nano/build.sh
@@ -4,8 +4,8 @@ set -e
 ./configure --prefix=/usr     \
             --sysconfdir=/etc \
             --enable-utf8     \
-            --docdir=/usr/share/doc/nano-8.6
+            --docdir=/usr/share/doc/nano-$MINIMAL_ARG_VERSION
 make -j$(nproc)
 
 make DESTDIR="$OUTPUT_DIR" install
-install -v -m644 doc/{nano.html,sample.nanorc} $OUTPUT_DIR/usr/share/doc/nano-8.6
+install -v -m644 doc/{nano.html,sample.nanorc} "${OUTPUT_DIR}/usr/share/doc/nano-${MINIMAL_ARG_VERSION}"
