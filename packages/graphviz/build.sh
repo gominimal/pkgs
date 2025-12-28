e657f748710bdd9a75aa540ed54e6585a5faea87
Tom <tom@minimal.dev>
2025-12-28 12:42:12 -0800

    graphviz: package graphviz version 14.1.1

---
+++ packages/graphviz/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+sed '/ORIGIN/d' -i lib/CMakeLists.txt
+
+mkdir build && cd build
+
+cmake \
+    -D CMAKE_INSTALL_PREFIX=/usr        \
+    -D CMAKE_INSTALL_LIBDIR=/usr/lib    \
+    -D CMAKE_BUILD_TYPE=Release         \
+    ..
+
+sed -i '/GZIP/s/:.*$/=/' CMakeCache.txt
+
+make -j$(nproc)
+
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${OUTPUT_DIR}/usr/lib" DESTDIR=$OUTPUT_DIR make install
