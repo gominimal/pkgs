046ab8d70cb9642e2a47c0f69699fe31e04e73e1
jake <jake@yrg.io>
2026-01-27 14:25:39 -0800

    2026-01-27 - added terraform

---
+++ packages/terraform/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -ex
+
+export GOROOT=/usr/go
+
+go build -ldflags "-w -s -X 'github.com/hashicorp/terraform/version.dev=no'" -o terraform .
+
+mkdir -p $OUTPUT_DIR/usr/bin
+install -m 755 terraform $OUTPUT_DIR/usr/bin/terraform
