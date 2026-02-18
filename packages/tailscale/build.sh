280a872cf1e4d3e73e5c460ccafaead283613388
jake <jake@yrg.io>
2026-02-17 21:43:58 -0800

    relocated env vars to pkgs from harnesses

---
+++ packages/tailscale/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ex
+
+export GOROOT=/usr/go
+export GONOSUMCHECK=*
+export GONOSUMDB=*
+
+go build -ldflags "-w -s -X 'tailscale.com/version.shortStamp=${MINIMAL_ARG_VERSION}' -X 'tailscale.com/version.longStamp=${MINIMAL_ARG_VERSION}'" -o tailscale ./cmd/tailscale
+go build -ldflags "-w -s -X 'tailscale.com/version.shortStamp=${MINIMAL_ARG_VERSION}' -X 'tailscale.com/version.longStamp=${MINIMAL_ARG_VERSION}'" -o tailscaled ./cmd/tailscaled
+
+mkdir -p $OUTPUT_DIR/usr/bin $OUTPUT_DIR/usr/sbin
+install -m 755 tailscale $OUTPUT_DIR/usr/bin/tailscale
+install -m 755 tailscaled $OUTPUT_DIR/usr/sbin/tailscaled
