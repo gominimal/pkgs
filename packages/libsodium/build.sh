7aef13e85cde93409ecfd6cfa491cc900074b9ec
Tom <tom@minimal.dev>
2026-02-17 16:58:35 -0800

    libsodium: fix source & deps

--- packages/libsodium/build.sh
+++ packages/libsodium/build.sh
@@ -4,7 +4,6 @@ set -ex
 export CFLAGS="-march=x86-64-v3 -O2 -pipe"
 export CXXFLAGS="${CFLAGS}"
 
-autoreconf -fi
 ./configure --prefix=/usr --disable-static
 make -j$(nproc)
 make DESTDIR=$OUTPUT_DIR install
