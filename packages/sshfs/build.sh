9e504f1ea08507d5e8e6771caebbc1f40f16571f
Tom <tom@minimal.dev>
2026-02-27 14:15:44 -0800

    sshfs: package sshfs version 3.7.5

---
+++ packages/sshfs/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+mkdir build &&
+cd    build
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export CFLAGS="$MARCH -O2 -pipe"
+export CXXFLAGS="${CFLAGS}"
+
+meson setup --prefix=/usr --buildtype=release ..
+ninja
+
+DESTDIR="$OUTPUT_DIR" ninja install
