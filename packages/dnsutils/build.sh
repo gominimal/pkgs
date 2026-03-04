9d5d23d5f8bee326403d3300fd2d3d9606abf6c4
Tom <tom@ciphersink.net>
2026-03-03 22:28:16 -0800

    dnsutils,liburcu: package dnsutils version 9.20.20

---
+++ packages/dnsutils/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -ex
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export CFLAGS="$MARCH -O2 -pipe"
+export CXXFLAGS="${CFLAGS}"
+
+tar --no-same-owner -xf bind-${MINIMAL_ARG_VERSION}.tar.xz
+cd bind-${MINIMAL_ARG_VERSION}
+
+./configure --prefix=/usr       \
+  --disable-static              \
+  --with-openssl                \
+  --without-lmdb                \
+  --disable-geoip               \
+  --without-json-c              \
+  --without-libxml2             \
+  --disable-doh
+
+make -j$(nproc)
+
+# Install everything, then keep only client binaries.
+# Output globs in build.ncl select only dig/host/nslookup/nsupdate.
+DESTDIR=$OUTPUT_DIR make install
