cea6876f94a07f884d122300b5b17c7ab556ce1c
Tom <tom@ciphersink.net>
2026-03-04 09:31:17 -0800

    pasta: package pasta version 2026_01_20.386b5f5

---
+++ packages/pasta/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export CC=gcc
+export CFLAGS="$MARCH -O2 -pipe"
+
+make CC=gcc -j$(nproc)
+
+mkdir -p $OUTPUT_DIR/usr/bin
+cp passt $OUTPUT_DIR/usr/bin/
+cp -a pasta $OUTPUT_DIR/usr/bin/
