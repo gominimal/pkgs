15742efdf3b9f167a9178af6cc0ccb9ac2ad3e3b
Tom <tom@ciphersink.net>
2026-03-08 21:44:47 -0700

    ruby: package ruby version 4.0.1

---
+++ packages/ruby/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+tar -xof ruby-4.0.1.tar.gz
+cd ruby-4.0.1
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export CFLAGS="$MARCH -O3 -pipe"
+export CXXFLAGS="${CFLAGS}"
+
+./configure --prefix=/usr \
+  --enable-shared \
+  --disable-install-doc \
+  --disable-install-rdoc
+
+make -j$(nproc)
+make DESTDIR=$OUTPUT_DIR install
