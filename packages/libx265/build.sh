dab7afc3d340ed80ce350a47cbf8380a2ab92f02
Tom <tom@ciphersink.net>
2026-03-11 10:50:57 -0700

    libx265: package libx265 version 4.1

---
+++ packages/libx265/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -ex
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export CFLAGS="$MARCH -O2 -pipe"
+export CXXFLAGS="${CFLAGS}"
+
+cd source
+
+# Remove deprecated cmake_policy(SET ... OLD) calls unsupported by modern CMake
+sed -i '/cmake_policy.*OLD/d' CMakeLists.txt
+
+cmake -B build \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+  -DENABLE_SHARED=ON \
+  -DENABLE_CLI=ON
+
+make -C build -j$(nproc)
+make -C build DESTDIR="$OUTPUT_DIR" install
