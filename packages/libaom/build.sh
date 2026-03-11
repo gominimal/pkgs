ebd5926e08ac884d5ae79591664c2cbd9d665218
Tom <tom@ciphersink.net>
2026-03-11 10:02:57 -0700

    libaom: package libaom version 3.13.1

---
+++ packages/libaom/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -ex
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export CFLAGS="$MARCH -O2 -pipe"
+export CXXFLAGS="${CFLAGS}"
+
+mkdir _build && cd _build
+
+cmake \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DBUILD_SHARED_LIBS=ON \
+  -DAOM_TARGET_CPU=generic \
+  -DENABLE_DOCS=OFF \
+  -DENABLE_TESTS=OFF \
+  -DENABLE_EXAMPLES=OFF \
+  ..
+
+make -j$(nproc)
+make DESTDIR="$OUTPUT_DIR" install
