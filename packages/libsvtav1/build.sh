f8f732e55bb9cbbec5a7fe7270db39f1f21b9657
Tom <tom@ciphersink.net>
2026-03-11 10:50:11 -0700

    libsvtav1: package libsvtav1 version 4.0.1

---
+++ packages/libsvtav1/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -ex
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export CFLAGS="$MARCH -O2 -pipe"
+export CXXFLAGS="${CFLAGS}"
+
+mkdir _build && cd _build
+
+cmake \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DBUILD_SHARED_LIBS=ON \
+  -DBUILD_TESTING=OFF \
+  -DBUILD_APPS=ON \
+  ..
+
+make -j$(nproc)
+make DESTDIR="$OUTPUT_DIR" install
