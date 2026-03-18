c6a66e8b6d04a3d768fdda3f6cf1eac384ab97e4
Tom <tom@ciphersink.net>
2026-03-18 10:30:50 -0700

    pulumi: install language runtimes

--- packages/pulumi/build.sh
+++ packages/pulumi/build.sh
@@ -3,7 +3,15 @@ set -ex
 
 export GOROOT=/usr/go
 
-go build -C pkg -ldflags "-w -s -X 'github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${MINIMAL_ARG_VERSION}'" -o ../pulumi github.com/pulumi/pulumi/pkg/v3/cmd/pulumi
+LDFLAGS="-w -s -X 'github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${MINIMAL_ARG_VERSION}'"
+
+go build -C pkg -ldflags "$LDFLAGS" -o ../pulumi github.com/pulumi/pulumi/pkg/v3/cmd/pulumi
+go build -C sdk/go/pulumi-language-go -ldflags "$LDFLAGS" -o pulumi-language-go .
+go build -C sdk/nodejs/cmd/pulumi-language-nodejs -ldflags "$LDFLAGS" -o pulumi-language-nodejs .
+go build -C sdk/python/cmd/pulumi-language-python -ldflags "$LDFLAGS" -o pulumi-language-python .
 
 mkdir -p $OUTPUT_DIR/usr/bin
 install -m 755 pulumi $OUTPUT_DIR/usr/bin/pulumi
+install -m 755 sdk/go/pulumi-language-go/pulumi-language-go $OUTPUT_DIR/usr/bin/pulumi-language-go
+install -m 755 sdk/nodejs/cmd/pulumi-language-nodejs/pulumi-language-nodejs $OUTPUT_DIR/usr/bin/pulumi-language-nodejs
+install -m 755 sdk/python/cmd/pulumi-language-python/pulumi-language-python $OUTPUT_DIR/usr/bin/pulumi-language-python
