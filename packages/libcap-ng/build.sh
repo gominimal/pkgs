34a30aa685c65b02a63d927bc1ddc0b0ca4eb871
ms <ms@localhost>
2026-04-02 12:51:05 -0700

    new pkgs: libcap-ng and libseccomp

---
+++ packages/libcap-ng/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -ex
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export SOURCE_DATE_EPOCH=0
+export CFLAGS="$MARCH -O2 -pipe -gno-record-gcc-switches -ffile-prefix-map=$(pwd)=/builddir"
+export LDFLAGS="-Wl,--build-id=none"
+export CXXFLAGS="${CFLAGS}"
+
+# GitHub tarballs don't include a pre-generated configure script
+# Create required automake files not included in the git archive
+touch NEWS
+autoreconf -fiv
+./configure --prefix=/usr --disable-static --without-python --without-python3
+make -j$(nproc)
+make DESTDIR=$OUTPUT_DIR install
