946f1c9fec3997c1fc554ea95d2d00051ef8c1cd
msample <4896732+msample@users.noreply.github.com>
2026-04-06 20:29:11 -0700

    cloud-hypervisor package: compiles on aarch64 now.  Was trying to inlcude an Intel/amd64-only feature on the aarch64 build

--- packages/cloud-hypervisor/build.sh
+++ packages/cloud-hypervisor/build.sh
@@ -8,7 +8,13 @@ export CARGO_INCREMENTAL=0
 export OPENSSL_DIR=/usr
 export OPENSSL_NO_VENDOR=1
 
-cargo build --release --features "kvm,io_uring,tdx,guest_debug,ivshmem,pvmemcontrol,fw_cfg"
+FEATURES="kvm,io_uring,guest_debug,ivshmem,pvmemcontrol,fw_cfg"
+
+if [ "$(uname -m)" = "x86_64" ]; then
+  FEATURES="${FEATURES},tdx"
+fi
+
+cargo build --release --features "$FEATURES"
 
 mkdir -p $OUTPUT_DIR/usr/bin
 cp target/release/cloud-hypervisor $OUTPUT_DIR/usr/bin/
