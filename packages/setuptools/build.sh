f0d9b9c43c90a1f5dfff131e2863c6222633d5db
Tom <tom@ciphersink.net>
2026-04-17 20:34:06 -0700

    setuptools: bump version to 82.0.1

--- packages/setuptools/build.sh
+++ packages/setuptools/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-tar -xof setuptools-80.9.0.tar.gz
-cd setuptools-80.9.0
+tar -xof "setuptools-$MINIMAL_ARG_VERSION.tar.gz"
+cd "setuptools-$MINIMAL_ARG_VERSION"
 
 pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
 pip3 install --root $OUTPUT_DIR --no-index --find-links dist setuptools
