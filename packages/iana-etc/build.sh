b48ee5494f1fdb2d86dc549b20c1e6ee82965876
bryan-minimal <bryan@minimal.dev>
2026-05-04 09:10:12 -0700

    Update iana-etc to 20260424 (#145)

--- packages/iana-etc/build.sh
+++ packages/iana-etc/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-tar -xof iana-etc-20250618.tar.gz
-cd iana-etc-20250618
+tar -xof iana-etc-20260424.tar.gz
+cd iana-etc-20260424
 
 mkdir -p "$OUTPUT_DIR/etc"
 cp services protocols "$OUTPUT_DIR/etc/"
