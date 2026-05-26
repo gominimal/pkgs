352d827b680337d2e39255328bd642daed06a37e
norrietaylor <me@norrietaylor.com>
2026-05-26 14:11:58 -0700

    Adding cmatrix pkg

---
+++ packages/cmatrix/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+autoreconf -fi
+
+case $(uname -m) in
+  x86_64)  MARCH="-march=x86-64-v3" ;;
+  aarch64) MARCH="-march=armv8-a" ;;
+  *)       MARCH="" ;;
+esac
+export CFLAGS="$MARCH -O2 -pipe -gno-record-gcc-switches -ffile-prefix-map=$(pwd)=/builddir"
+export LDFLAGS="-Wl,--build-id=none"
+
+./configure --prefix=/usr
+
+make -j$(nproc) LIBS="-lncursesw"
+
+make DESTDIR="$OUTPUT_DIR" install
