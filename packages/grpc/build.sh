fc6dc87b26755cf21973513a063722beab5579d0
Lovel Rishi <lovelworks@gmail.com>
2026-08-02 11:54:39 +0000

    Add grpc package (v1.83.0)

---
+++ packages/grpc/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+
+export CFLAGS="-O3 -pipe -ffile-prefix-map=$(pwd)=/builddir -gno-record-gcc-switches"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-Wl,--build-id=none"
+export ARFLAGS=Drc
+
+cmake -B build \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DCMAKE_INSTALL_LIBDIR=/usr/lib \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=ON \
+  -DgRPC_INSTALL=ON \
+  -DgRPC_BUILD_TESTS=OFF \
+  -DgRPC_ABSL_PROVIDER=package \
+  -DgRPC_CARES_PROVIDER=package \
+  -DgRPC_PROTOBUF_PROVIDER=package \
+  -DgRPC_RE2_PROVIDER=package \
+  -DgRPC_SSL_PROVIDER=package \
+  -DgRPC_ZLIB_PROVIDER=package \
+  -DCMAKE_CXX_STANDARD=14
+make -C build -j$(nproc)
+make DESTDIR=$OUTPUT_DIR -C build install
