8556f4e3abef2b7d0fe48603c1cc44a06c01c917
Nina Petrova <npetrovcode@users.noreply.github.com>
2026-08-26 19:27:41 +0300

    fix: stamp Biome version at build time

--- packages/biome/build.sh
+++ packages/biome/build.sh
@@ -3,6 +3,7 @@ set -ex
 
 export CC=gcc
 export LD=gcc
+export BIOME_VERSION="$MINIMAL_ARG_VERSION"
 export RUSTFLAGS="-C linker=gcc --remap-path-prefix=$(pwd)=/builddir --remap-path-prefix=$HOME/.cargo=/cargo"
 
 cargo build --release -p biome_cli
