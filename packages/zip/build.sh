8fabe3c18982cb63fa54f4ddb61c45ca9bf13582
bryan <bryan@minimal.dev>
2026-09-22 09:24:08 -0700

    zip: -std=gnu89 (K&R sources; C23 gcc breaks its configure probes)

--- packages/zip/build.sh
+++ packages/zip/build.sh
@@ -5,7 +5,8 @@ case $(uname -m) in
   aarch64) MARCH="-march=armv8-a" ;;
   *)       MARCH="" ;;
 esac
-export CFLAGS="$MARCH -O2 -pipe -gno-record-gcc-switches -ffile-prefix-map=$(pwd)=/builddir"
+# Info-ZIP is K&R C: under a C23-default gcc its configure probes fail and zip.h then redeclares memset/memcpy.
+export CFLAGS="$MARCH -O2 -pipe -std=gnu89 -Wno-implicit-function-declaration -gno-record-gcc-switches -ffile-prefix-map=$(pwd)=/builddir"
 export LDFLAGS="-Wl,--build-id=none"
 # unix/Makefile's `generic` target runs its own feature probes; pass the flags through its variables.
 make -f unix/Makefile generic CC=gcc CFLAGS_NOOPT="$CFLAGS -DUNIX -I." LFLAGS1="$LDFLAGS"
